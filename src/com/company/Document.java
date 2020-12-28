9c48ca928d9f14af499c7bc717aa0628e7c00f9a
Irina Ezerskaya <titira217@mail.ru>
2020-12-28 22:53:33 +0300

    Initial commit

--- src/com/company/Document.java
+++ src/com/company/Document.java
@@ -1,2 +1,19 @@
-package com.company;public class Document {
+package com.company;
+
+// Класс, описывающий вводимые документы
+public class Document {
+
+    public String type;
+    public String info;
+
+    // Конструктор класса
+    public Document(String type, String info) {
+        this.type = type;
+        this.info = info;
+    }
+
+    @Override
+    public String toString() {
+        return "Документ "+type+", доп инфо: "+info;
+    }
 }
